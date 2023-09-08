9edb4a101b374972e521ba508025fef556036f1b
Ansh <AnshM_25@outlook.com>
2023-09-08 23:27:16 +0530

    Started a new JavaScript repo

---
+++ 01_consoleMethods.js
@@ -0,0 +1,19 @@
+// All of these also work directly in the console. Press F12 to open the console.
+
+console.log("Hello World");
+
+console.log(10,"Hello",true);
+
+alert("There's a problem.");
+
+console.error("Only being displayed in the console.");
+
+console.log(new Date());
+
+console.table({name:"John", email:"JohnDoe@gmail.com", age:23});
+
+//clear() clear's the terminal
+
+const styles = "padding: 10px; background-color: white; color: green";
+
+console.log("%cHello World",styles);
