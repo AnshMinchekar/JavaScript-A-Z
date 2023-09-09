48614f3638c2ca4a6e87b5e6709da9b3a5197d8b
Ansh <AnshM_25@outlook.com>
2023-09-09 23:07:53 +0530

    Added DataTypes section

---
+++ 03_DataTypes.js
@@ -0,0 +1,28 @@
+// Primitive Data Types
+
+// 1. String
+// 2. Number
+// 3. Boolean
+// 4. Null 
+// 5. Undefined
+// 5. Symbol
+// 6. BigInt 
+
+//Reference Types (Objects)
+
+// 1. Object Literals
+// 2. Arrays
+
+const firstName = "John"
+console.log(typeof firstName);
+
+const age = 23;
+console.log(typeof age);
+
+const address = null;
+console.log(typeof address); //This will return as an object but it is a mistake.
+
+const phoneNumber = undefined;
+console.log(typeof phoneNumber);
+
+
